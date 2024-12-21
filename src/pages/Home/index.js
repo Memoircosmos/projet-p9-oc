f0f2f4d827e6f574487227b611c9816af3c1fba4
Maxime Galarreta <crusix@msn.com>
2024-12-21 18:19:45 +0100

    condition dans le cas ou last est indéfini

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -124,6 +124,7 @@ const Page = () => {
     <footer className="row">
       <div className="col presta">
         <h3>Notre derniére prestation</h3>
+        {last && 
         <EventCard
           imageSrc={last?.cover}
           title={last?.title}
@@ -131,6 +132,7 @@ const Page = () => {
           small
           label="boom"
         />
+        }
       </div>
       <div className="col contact">
         <h3>Contactez-nous</h3>
